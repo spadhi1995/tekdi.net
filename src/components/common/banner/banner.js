70e7e7d5763e9b9a3215a323b9c1d147b450fd83
Pallavi Patil <pallavi_p@tekditechnologies.com>
2020-01-29 00:41:35 +0530

    Issue #29 feat: Products & Platforms page design

--- src/components/common/banner/banner.js
+++ src/components/common/banner/banner.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PreviewCompatibleImage from '../preview-compatible-image';
 import './banner.scss';
 
 const Banner = props => {
     return(
         <div className="banner">
-        <div className="container">
-          <div className="banner-text">
-              <p className="text-white font-weight-normal">{props.bannerSubTitle}</p>
-              <h1 className="text-white">{props.bannerTitle}</h1>
+          <PreviewCompatibleImage
+            imageInfo={{
+              image: props.bannerbgimage,
+              alt: `banner image ${props.title}`,
+            }}
+          />
+          <div className="container">
+            <div className="banner-text">
+                <p className="text-white font-weight-normal">{props.bannerSubTitle}</p>
+                <h1 className="text-white">{props.bannerTitle}</h1>
+            </div>
           </div>
-        </div>
       </div>
     );
 }
