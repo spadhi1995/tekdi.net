f423de927a3b12de6ee3d36b8b75b3a676380311
Pallavi Patil <pallavi_p@tekditechnologies.com>
2020-01-29 15:19:47 +0530

    Issue #30 chore: Menus responsive & update home industries image

--- src/components/home-about-us/about-us.js
+++ src/components/home-about-us/about-us.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React  from 'react';
 import PropTypes from 'prop-types';
 import { Link, graphql, StaticQuery } from 'gatsby';
 import './about-us.scss';
